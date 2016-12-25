f8be0d29c5413a198457b31e042212ffebc7b04a
judgewooden <douwe.jong@gmail.com>
2016-12-25 21:55:55 +0100

    🎟🎅 Updated with Gomix

--- public/client.js
+++ public/client.js
@@ -7,6 +7,7 @@
 $(function() {
   console.log('hello world :o');
   
+  
   $.get('/dreams', function(dreams) {
     dreams.forEach(function(dream) {
       $('<li></li>').text(dream).appendTo('ul#dreams');
