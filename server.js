57b056f818fccafc2acd91e2ddfd6cf28f101bc1
judgewooden <douwe.jong@gmail.com>
2016-12-26 23:54:03 +0100

    🚝🚪 Updated with Gomix

--- server.js
+++ server.js
@@ -16,10 +16,6 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
